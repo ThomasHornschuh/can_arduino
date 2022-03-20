b076942c763a446d615066014ec2f02e8da3ff47
Thomas Hornschuh <thomas@bonfirecpu.eu>
2022-03-20 19:37:51 +0100

    Lot of changes

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,55 @@ const int CAN_INT_PIN = 2;
 
 const byte myID = 0x0;
 
+const uint32_t responseWaitTimeout = 30; // 30ms 
+
+enum hitec_opcodes  {
+   reg_pos = 0x0c,
+   reg_torque = 0x10,
+   reg_voltage = 0x12,
+   reg_temp = 0x14,
+   reg_setpos = 0x1e
+};
+
+enum servo_pos {
+   servo_left = 700,
+   servo_neutral = 3000,
+   servo_right = 5300
+};
+
 static volatile int flagRecv = 0;
 
 void MCP2515_ISR() {
    flagRecv = 1;
 }
 
-void setup() {
-   Serial.begin(115200);
-   while(!Serial){};
-  
-   attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING);
-   // init can bus : baudrate = 500k
-   while (CAN_OK != CAN.begin(CAN_1000KBPS,MCP_8MHz)) {
-      Serial.println("CAN init fail, retry...");
-      delay(100);
+
+
+uint16_t readServoValue(enum hitec_opcodes op) {
+byte msgbuf[7];
+
+
+   msgbuf[0]=0x96; msgbuf[1] = 0;
+   msgbuf[2] = op; msgbuf[3] = 0;
+   msgbuf[4] = (byte)op; // Checksum
+
+   byte res=CAN.sendMsgBuf(myID,0,5, msgbuf);
+   if (res==CAN_OK) {
+      uint32_t timeout = millis() + responseWaitTimeout;
+      while(!flagRecv) {
+         if (millis()>timeout) return 0; // Avoid endless loop when there os no response from servo
+      }; 
+      flagRecv=0;
+      if (CAN_MSGAVAIL == CAN.checkReceive()) {
+          byte len;
+          CAN.readMsgBuf(&len,msgbuf);
+          if (len>0 && msgbuf[2]==op && msgbuf[3]==2) {
+             return (uint16_t)(msgbuf[4] | msgbuf[5] << 8);               
+          }
+      } 
    }
-   Serial.println("CAN init ok!");
-   CAN.setSleepWakeup(true);
-   CAN.setMode(MODE_NORMAL);
-   //CAN.setMode(MODE_LOOPBACK);
+   return 0;
 }
-
-
 
 
 void printMessage(byte *msg, byte len)
@@ -51,6 +77,22 @@ byte sendAndPrint(byte *msg,byte len)
    Serial.print(F("SendMessage returns: "));Serial.println(res,HEX);
    return res;
 }
+
+
+byte setServoPos(uint16_t pos)
+{
+   byte msgbuf[7];
+
+
+   msgbuf[0]=0x96; msgbuf[1] = 0;
+   msgbuf[2] = reg_setpos; msgbuf[3] = 2;
+   msgbuf[4] = pos & 0xff; msgbuf[5] = pos >> 8;
+   byte chksum = 0;
+   for(byte i=1;i<=5;i++) chksum+=msgbuf[i];
+   msgbuf[6] = chksum;
+   return sendAndPrint(msgbuf,7);
+}
+
 
 void checkReceive() {
    byte recvMsg[16];
@@ -72,28 +114,72 @@ void checkReceive() {
    }
 }
 
-byte readpos[] = {0x96,0,0x0c,0,0x0c};
-byte readvoltage[] = {0x96,0,0x12,0,0x012};
-byte neutral[] = {0x96,0,0x1e,2,0xb8,0x0b,0xe3};
-byte left[] = {0x96,0,0x1e,2,0x58,0x02,0x7a};
+
+void trackPos(uint32_t time,uint16_t target,uint16_t delta)
+{
+uint32_t s = millis();  
+
+   Serial.print(F("tracking Target: "));Serial.println(target);
+   while (millis()-s < time ) {
+       //delay(20);
+       uint16_t v=readServoValue(reg_pos);
+       if (v==0) return; // Servo not responding...
+       Serial.print(" ");Serial.print(v);
+       if (abs(target-v)<delta) return; // Target position reached
+
+   }  
+}
+
+void printInfo()
+{
+   Serial.print("Servo Voltage: ");Serial.print((float)readServoValue(reg_voltage)/100);Serial.println("V");
+   Serial.print("Servo Temp: ");Serial.print(readServoValue(reg_temp));Serial.println("C");   
+}
+
+
+void setup() {
+   Serial.begin(115200);
+   while(!Serial){};
+   Serial.println(F("CANArduino 0.0"));
+  
+   attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING);
+   // init can bus : baudrate = 500k
+   while (CAN_OK != CAN.begin(CAN_1000KBPS,MCP_8MHz)) {
+      Serial.println("CAN init fail, retry...");
+      delay(100);
+   }
+   Serial.println("CAN init ok!");
+   CAN.setSleepWakeup(true);
+   CAN.setMode(MODE_NORMAL);
+   printInfo();
+   setServoPos(servo_neutral);
+   delay(3000);
+   //CAN.setMode(MODE_LOOPBACK);
+}
+
 
 void loop() {
 
     if (CAN.getMode()==MODE_LOOPBACK) {
        Serial.println(F("\nLoopback test"));
-       sendAndPrint(neutral,sizeof(neutral));
+       setServoPos(servo_neutral);
        checkReceive();
        delay(1000);
     }
     else {
       
-      byte res=sendAndPrint(readvoltage,sizeof(readvoltage)); 
-      if (res==CAN_OK) checkReceive();    
+      // byte res=sendAndPrint(readvoltage,sizeof(readvoltage)); 
+      // if (res==CAN_OK) checkReceive();    
+     
+      printInfo();
+      setServoPos(servo_left);
+      trackPos(100,servo_left,100);
       delay(1000);
-      // sendAndPrint(neutral,sizeof(neutral));
-      // delay(1000);
-      // sendAndPrint(left,sizeof(left));
-      // delay(1000);
+      Serial.println();
+      setServoPos(servo_right);
+      trackPos(100,servo_right,100);
+      delay(1000);
+      Serial.println();
 
     }
 
